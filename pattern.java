e0d5e67057b85dc5b954b45bc82fecbd2c494f95
Rushikesh Shankar Gaikwad <112718662+gaikwadrushi@users.noreply.github.com>
2024-01-16 21:52:07 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class pattern
+{
+public static void main(String args[])
+{
+int n,i,j;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the no.of rows");
+n=sc.nextInt();
+for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=i;j++)
+			{
+				System.out.print("j");
+			}
+		
+		System.out.println();
+	}
+}
+}
